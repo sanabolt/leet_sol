3e7e6a50a1edfd4f91bffee848b59631aa9fa42e
Sana Bansod <sanabansod23@gmail.com>
2026-09-25 22:16:40 +0530

    Time: 37 ms (73.98%) | Memory: 81.3 MB (81.88%) - LeetSync

--- 1352-maximum-profit-in-job-scheduling/maximum-profit-in-job-scheduling.cpp
+++ 1352-maximum-profit-in-job-scheduling/maximum-profit-in-job-scheduling.cpp
@@ -1,58 +1,39 @@
 class Solution {
 public:
-    vector<vector<int>> jobs;
-    vector<int> dp;
-
-    int solve(int i) {
-        if (i == jobs.size())
-            return 0;
-
-        if (dp[i] != -1)
-            return dp[i];
-
-        // Skip current job
-        int skip = solve(i + 1);
-
-        // Binary Search
-        int low = i + 1;
-        int high = jobs.size();
-
-        while (low < high) {
-            int mid = low + (high - low) / 2;
-
-            if (jobs[mid][0] >= jobs[i][1])
-                high = mid;
-            else
-                low = mid + 1;
-        }
-
-        int next = low;
-
-        // Take current job
-        int take = jobs[i][2] + solve(next);
-
-        return dp[i] = max(take, skip);
-    }
-
-    int jobScheduling(vector<int>& startTime,
-                      vector<int>& endTime,
-                      vector<int>& profit) {
-
+    int jobScheduling(vector<int>& startTime, vector<int>& endTime, vector<int>& profit) {
+        
         int n = startTime.size();
 
+        // {start, end, profit}
+        vector<array<int, 3>> jobs;
+
         for (int i = 0; i < n; i++) {
-            jobs.push_back({
-                startTime[i],
-                endTime[i],
-                profit[i]
-            });
+            jobs.push_back({startTime[i], endTime[i], profit[i]});
         }
 
-        // Sort by start time
+        // Sort jobs by starting time
         sort(jobs.begin(), jobs.end());
 
-        dp.assign(n, -1);
+        vector<int> dp(n + 1, 0);
 
-        return solve(0);
+        for (int i = n - 1; i >= 0; i--) {
+
+            // Find first job whose start >= current job's end
+            int next = lower_bound(
+                jobs.begin() + i + 1,
+                jobs.end(),
+                array<int, 3>{jobs[i][1], 0, 0}
+            ) - jobs.begin();
+
+            // Take current job
+            int take = jobs[i][2] + dp[next];
+
+            // Skip current job
+            int skip = dp[i + 1];
+
+            dp[i] = max(take, skip);
+        }
+
+        return dp[0];
     }
 };
